13cff4d40163074d344b03cb49051779f47147bf
Mileto Di Marco <mileto.di.marco@howest.be>
2020-10-05 10:45:10 +0200

    adds links to homepage

---
+++ Wba.Oefening.Games.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wba.Oefening.Games.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //zonder stringbuilder
+            string content = "<h2>Rate-a-Game</h2>";
+            content += "<a href='games/index'>Games</a> || ";
+            content += "<a href='developers/index'>Developers</a>";
+            return Content(content,"text/html");
+        }
+    }
+}
