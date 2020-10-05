4fc84a3afd1aef8328a1a3e1fafb7191f2adf56e
Mileto Di Marco <mileto.di.marco@howest.be>
2020-10-05 09:40:39 +0200

    adds comments to developercontroller

---
+++ Wba.Oefening.Games.Web/Controllers/DeveloperController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wba.Oefening.Games.Web.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        //declare DEveloperRepository property
+
+        //constructor initialisation
+
+        //action methods
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
